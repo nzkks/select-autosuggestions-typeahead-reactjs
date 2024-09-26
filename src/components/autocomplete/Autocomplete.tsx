f00487ad2e8d514f03fb917216f7d9fcff90cc59
nzkks <kkshanthosh@gmail.com>
2024-09-26 16:14:57 +1200

    Autocomplete: handle onChange event

--- src/components/autocomplete/Autocomplete.tsx
+++ src/components/autocomplete/Autocomplete.tsx
@@ -1,11 +1,19 @@
+import { ChangeEvent, useState } from 'react';
+
 type Props = {
   placeholder: string;
 };
 
 const Autocomplete = ({ placeholder = '' }: Props) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="container">
-      <input type="text" placeholder={placeholder} />
+      <input type="text" value={inputValue} onChange={handleOnChange} placeholder={placeholder} />
     </div>
   );
 };
