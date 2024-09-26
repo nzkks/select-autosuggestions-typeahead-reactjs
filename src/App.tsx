2a2b298ebbe04154a525765c51fa1909cf046ada
nzkks <kkshanthosh@gmail.com>
2024-09-26 17:59:58 +1200

    fetch suggestions on entering a search term

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import './App.css';
 import Autocomplete from './components/autocomplete/Autocomplete';
 
 const App = () => {
+  const handleFetchSuggestions = async (query: string) => {
+    const response = await fetch(`https://dummyjson.com/recipes/search?q=${query}`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const result = await response.json();
+    return result.recipes;
+  };
+
   return (
     <>
       <h3>Select with auto-suggesstions and typeahead</h3>
 
-      <Autocomplete placeholder="Enter recipe name..." />
+      <Autocomplete placeholder="Enter recipe name..." fetchSuggestions={handleFetchSuggestions} />
     </>
   );
 };
--- src/components/autocomplete/Autocomplete.tsx
+++ src/components/autocomplete/Autocomplete.tsx
@@ -1,15 +1,55 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 type Props = {
   placeholder: string;
+  fetchSuggestions?: (query: string) => Promise<[]>;
 };
 
-const Autocomplete = ({ placeholder = '' }: Props) => {
+const Autocomplete = ({ placeholder = '', fetchSuggestions }: Props) => {
   const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+
+  const getSuggestions = async (query: string) => {
+    setError('');
+    setIsLoading(true);
+    try {
+      let result;
+
+      if (fetchSuggestions) {
+        result = await fetchSuggestions(query);
+      }
+
+      if (result) {
+        console.log(result);
+        setSuggestions(result);
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('Unknown error');
+      }
+
+      setSuggestions([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (inputValue.length > 1) {
+      getSuggestions(inputValue);
+    } else {
+      setSuggestions([]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValue]);
 
   return (
     <div className="container">
